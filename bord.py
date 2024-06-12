2926b1193d17b738972599cf3a87aa1f005d9ce7
jhenriqueax <joao.henrique.xavier@ccc.ufcg.edu.br>
2024-06-12 12:17:24 -0300

    feat: add print board

--- bord.py
+++ bord.py
@@ -32,22 +32,38 @@ class GameBoard:
         self.center = ['X']
         self.stores = [ [] for _ in range(stores_qtd[qtd_players])]
         
-    def distribute_pieces(self, pieces):
+    def distribute_pieces(self, pieces) -> None:
         for store in self.stores:
             for _ in range(4):
                 store.append(pieces.pop())
     
-    def pick_piece(self, store_index: int, piece: str):
+    def pick_piece(self, store_index: int, piece: str) -> list:
+        piece = piece.upper()
+        
         if piece not in self.stores[store_index]:
             raise ValueError('Piece not in store')
         
         picked = []
-        while piece in self.stores[store_index]:
-            picked.append(self.stores[store_index].pop())
-        
-        self.center.extend(self.stores[store_index])
+        if store_index != 'C':
+            while piece in self.stores[store_index]:
+                picked.append(self.stores[store_index].pop(self.stores[store_index].index(piece)))
+            self.center.extend(self.stores[store_index])
+            self.stores[store_index] = []
+        else:
+            while piece in self.center:
+                if 'X' in self.center:
+                    picked.append('X')
+                    self.center.remove('X')
+                picked.append(self.center.pop())
+                
         return picked
             
+    def print_bord(self) -> None:
+        for i, store in enumerate(self.stores):
+            print(f'{i} | {store}')
+            
+        print('C |', self.center)
+        
         
 class Game:
     
@@ -57,15 +73,15 @@ class Game:
         self.current_player = 0
         self.game_board = GameBoard(qtd_players)
         
-    def start(self):
+    def start(self) -> None:
         random.shuffle(self.bag)
         self.game_board.distribute_pieces(self.bag)
         
-    def turn(self):
+    def turn(self) -> None:
         player = self.players[self.current_player]
-        print(f'Player {self.current_player + 1} Turn')
-        print(self.game_board.stores)
-        print(self.game_board.center)
+        print(f'Player {self.current_player + 1}')
+        print('-' * 20)
+        self.game_board.print_bord()
         pick = input('Pick a Piece | EX: (5U for U in Store 5): ')
         if len(pick) != 2:
             print('Invalid Input')
@@ -73,11 +89,10 @@ class Game:
         
         store_index, piece = pick
         store_index = int(store_index)
-        print()
         picked = self.game_board.pick_piece(store_index, piece)
+        print('- ' * 20)
         print(f'Picked: {picked}')
-        print(self.game_board.stores)
-        print(self.game_board.center)
+        print('- ' * 20)
         
         
 if __name__ == '__main__':
