7a2a7d006abb89c58b3a12243c0f7dad7ba31454
Mateus Matias <mateus.ribeiro@ccc.ufcg.edu.br>
2024-06-18 15:13:04 -0300

    revert: remove history

--- src/game.py
+++ src/game.py
@@ -4,7 +4,6 @@ import random
 
 from gameboard import GameBoard
 from playerboard import PlayerBoard
-from ai.minmax import MinMaxPlayer
 from util import print_boards
 
 
@@ -12,7 +11,6 @@ class Game:
     INITIAL_PIECES = ["🟥", "⬛", "🟩", "🟧", "🟦"]
     PIECES_PER_TYPE = 20
     MINIMUM_PIECES_IN_BAG = 4
-    HISTORY = []
 
     def __init__(self, qtd_players=2) -> None:
         self.bag = self.INITIAL_PIECES * self.PIECES_PER_TYPE
@@ -62,20 +60,6 @@ class Game:
         while not self.check_end_game():
             while not self.all_stores_empty():
                 self._update_display()
-                state = {
-                    "player": self.current_player,
-                    "p1_score": copy.deepcopy(self.players[0].score),
-                    "p2_score": copy.deepcopy(self.players[1].score),
-                    "build_tower": copy.deepcopy(
-                        self.players[self.current_player].build_tower
-                    ),
-                    "board": copy.deepcopy(self.players[self.current_player].board),
-                    "broken": copy.deepcopy(
-                        self.players[self.current_player].broken_pieces
-                    ),
-                    "stores": copy.deepcopy(self.game_board.stores),
-                    "center": copy.deepcopy(self.game_board.center),
-                }
                 pick = input(
                     "Pick a Piece | EX: (5U for U in Store 5, CU for U in Center): "
                 )
@@ -88,32 +72,26 @@ class Game:
                         print(e)
 
                 player = self.players[self.current_player]
-                place_lines = []
                 while picked:
                     self._update_display(picked)
                     line = input("Place pieces in the tower (line number): ")
                     picked = player.place_pieces_tower(picked, line)
-                    place_lines.append(line)
-                state["pick"] = pick
-                state["place_lines"] = place_lines
-                self.HISTORY.append(state)
                 self._advance_turn()
             self.used_pieces.extend(self.turn_placement())
             self.current_player = self.find_starting_player()
             self._refill_bag_if_needed()
             self.game_board.distribute_pieces(self.bag)
         self._end_game()
-        
+
     def play_game_ai(self) -> None:
         self.start()
         AI_INDEX = 1
-        AI_PLAYER = MinMaxPlayer(3, AI_INDEX)
         while not any(player.check_end_game() for player in self.players):
             while not self.all_stores_empty():
                 self._update_display()
                 if self.current_player != AI_INDEX:
                     pick = input(
-                    "Pick a Piece | EX: (5U for U in Store 5, CU for U in Center): "
+                        "Pick a Piece | EX: (5U for U in Store 5, CU for U in Center): "
                     )
                     was_picked = False
                     while not was_picked:
@@ -130,32 +108,13 @@ class Game:
                         picked = player.place_pieces_tower(picked, line)
                     self._advance_turn()
                 else:
-                    best_move = AI_PLAYER.get_best_move_2(self)
-                    print(best_move)
-                    # picked = self.turn_select(best_move[0])
-                    # player = self.players[self.current_player]
-                    # while picked:
-                    #     player.place_pieces_tower(picked, best_move[1])
-                    # self._advance_turn()
-                    
-            
+                    pass
 
-    def undo_move(self):
-        game_status = self.HISTORY[-1]
-        self.current_player = game_status["player"]
-        self.players[0].score = game_status["p1_score"]
-        self.players[1].score = game_status["p2_score"]
-        self.players[self.current_player].build_tower = game_status["build_tower"]
-        self.players[self.current_player].board = game_status["board"]
-        self.players[self.current_player].broken_pieces = game_status["broken"]
-        self.game_board.stores = game_status["stores"]
-        self.game_board.center = game_status["center"]
-        
     def check_end_game(self):
         return any(player.check_end_game() for player in self.players)
 
     def _update_display(self, picked=None) -> None:
-        #self._clear_screen()
+        self._clear_screen()
         self._display_game_status()
         self._display_turn_info()
         if picked:
@@ -202,5 +161,4 @@ class Game:
         return store_index, piece
 
     def _advance_turn(self) -> None:
-        print("-" * 20)
         self.current_player = (self.current_player + 1) % len(self.players)
